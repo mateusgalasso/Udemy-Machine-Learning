90cc0159a60fb90b12d39be9f574cc54f7da4836
stonkeep <mateusgalasso@yahoo.com.br>
2017-07-02 18:10:18 -0300

    seção 2 aula 15

--- R/data_preprocessing_template.R
+++ R/data_preprocessing_template.R
@@ -5,19 +5,19 @@
 dataset = read.csv('Data.csv')
 
 #take care of missing data
-dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Age) 
-
-dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
-                     dataset$Salary) 
+# dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Age) 
+# 
+# dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+#                      dataset$Salary) 
 
 #Enconding categorical data
-dataset$Country = factor(dataset$Country, 
-                         levels = c('France', 'Spain', 'Germany'),
-                         labels = c(1, 2, 3))
-dataset$Purchased = factor(dataset$Purchased, 
-                         levels = c('No', 'Yes'),
-                         labels = c(0, 1))
+# dataset$Country = factor(dataset$Country, 
+#                          levels = c('France', 'Spain', 'Germany'),
+#                          labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased, 
+#                          levels = c('No', 'Yes'),
+#                          labels = c(0, 1))
 
 #spliting the dataset into the Training set and Test set
 #install.packages('caTools')
@@ -26,3 +26,21 @@ set.seed(123)
 split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+#Fature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
