b42893c7b480c71e7de6d50a26e93c19579e09a9
stonkeep <mateusgalasso@yahoo.com.br>
2017-07-02 17:01:18 -0300

    second commit

---
+++ R/data_preprocessing_template.R
@@ -0,0 +1,12 @@
+# Data processing 
+
+#importing dataset
+
+dataset = read.csv('Data.csv')
+
+#take care of missing data
+dataset$Age = ifelse(is.na(dataset$Age), ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age) 
+
+dataset$Salary = ifelse(is.na(dataset$Salary), ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary) 
---
+++ data_preprocessing_template.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#data preprocessing
+
+#import the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import the dataset
